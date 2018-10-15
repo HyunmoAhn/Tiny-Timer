36ebeec85fed4283127dc292ec6272a4839abd96
hyunmoahn <mos_dev@naver.com>
2018-10-15 22:11:04 +0900

    Basic TrayWindow Setting using menubar.

--- app/index.js
+++ app/index.js
@@ -1,3 +1,19 @@
 import { app } from 'electron';
+import path from 'path';
+import url from 'url';
+import TrayBar from 'menubar';
 
-app.on('ready', () => console.log('Hello World'));
+const tray = TrayBar({
+  index: url.format({
+    pathname: path.resolve(__dirname, 'timer.html'),
+    protocol: 'file:',
+    slashes: true,
+  }),
+  icon: path.resolve(__dirname, 'assets/clockTemplate.png'),
+  width: 250,
+  height: 100,
+});
+
+app.on('ready', () => {
+  tray.showWindow();
+});
