74036b9e997b39a599bdb5420b97a6e7c77d2810
hyunmoahn <mos_dev@naver.com>
2018-10-16 21:25:45 +0900

    Basic View setting using javascript.

---
+++ app/timer.js
@@ -0,0 +1,15 @@
+function setTimer() {
+  return `<div>Set</div>`
+}
+
+function countTimer() {
+  return `<div>Count</div>`
+}
+
+function init() {
+  const root = document.getElementById('root');
+
+  root.innerHTML = setTimer();
+}
+
+init();
