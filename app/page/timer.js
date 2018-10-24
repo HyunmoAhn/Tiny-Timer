f522be22500f7475ccff4d14edb343ecc1324cf3
hyunmoahn <mos_dev@naver.com>
2018-10-25 06:05:01 +0900

    Add function to draw DOM.

--- app/page/timer.js
+++ app/page/timer.js
@@ -1,4 +1,4 @@
-import { remote } from 'electron';
+import cx from 'classnames';
 
 /**
  * Manager data of timer.
@@ -32,8 +32,65 @@ class Timer {
   }
 }
 
-function countTimer() {
-  return `<div>Count</div>`
+/**
+ * Update DOM about progress count.
+ * @param(Number) time: number of remain timer
+ */
+function renderProgressView(time) {
+  function progressView(number) {
+    const hour = parseInt(number / 3600);
+    const minute = parseInt((number - hour * 60) / 60);
+    const second = number % 60;
+
+    const hourClassName = cx('Progress__hour', { 'Progress__hour--hide': hour === 0 });
+    const minuteClassName = cx('Progress__minute', { 'Progress__minute--hide': minute === 0 && hour === 0 });
+    const secondClassName = cx('Progress__second');
+
+    return `
+    <div class="Progress__Container">
+      <span class=${hourClassName}>${hour}</span>
+      <span class=${minuteClassName}>${minute}</span>
+      <span class=${secondClassName}>${second}</span>
+    </div>
+  `;
+  }
+
+  const timerRoot = document.getElementById('timer-root');
+
+  timerRoot.innerHTML = progressView(time);
+}
+
+/**
+ * Update DOM about setting count.
+ * @param(Number) time: number of set timer
+ */
+function settingView(number) {
+  const hour = parseInt(number / 3600);
+  const minute = parseInt((number - hour * 60) / 60);
+  const second = number % 60;
+
+  return `
+    <form>
+      <input class="Setting__hour" autofocus placeholder="0" type="number" value=${hour}>
+      <span>:</span>
+      <input class="Setting__minute" placeholder="00" type="number" value=${minute}>
+      <span>:</span>
+      <input class="Setting__second" placeholder="00" type="number" value=${second}>
+      <input class="Setting__submit-btn" type="submit">
+    </form>
+  `;
+}
+
+/**
+ * DOM about comment for alert when time is zero.
+ * @returns {string}
+ */
+function commentView() {
+  return `
+    <div>
+      <input type="text" placeholder="Please type to alert message">
+    </div>
+  `;
 }
 
 function init() {
