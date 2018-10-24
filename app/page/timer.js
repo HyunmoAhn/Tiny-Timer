c2b112834e5a678f60288b6cbe25068b21701d1b
hyunmoahn <mos_dev@naver.com>
2018-10-25 06:03:03 +0900

    Add class for manage timer

--- app/page/timer.js
+++ app/page/timer.js
@@ -1,7 +1,35 @@
 import { remote } from 'electron';
 
-function setTimer() {
-  return `<div>Set!!</div>`
+/**
+ * Manager data of timer.
+ */
+class Timer {
+  constructor(time) {
+    this.time = time;
+    this.remain = this.time;
+    this.tick = null;
+  }
+
+  setTime(num) {
+    this.time = num;
+    this.remain = num;
+  }
+
+  getRemainTime() {
+    return this.remain;
+  }
+
+  runTimeProgress(cb) {
+    this.tick = setInterval(() => {
+      this.remain -= 1;
+      cb();
+    }, 1000);
+  }
+
+  stopTimeProgress() {
+    clearInterval(this.tick);
+    this.tick = null;
+  }
 }
 
 function countTimer() {
