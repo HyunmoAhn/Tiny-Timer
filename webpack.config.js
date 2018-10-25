4d14a4486b6403d65a7ebb8c67d5d69026fc9dc2
hyunmoahn <mos_dev@naver.com>
2018-10-25 15:13:35 +0900

    Config about scss using webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,14 @@ module.exports = {
         include: [path.join(__dirname, '/app/page')],
         exclude: /node_modules/,
         use: [{ loader: 'babel-loader' }]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
       }
     ]
   },
