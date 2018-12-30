40e975db95d893f84a7c545384a652b2b8c8b4aa
Hyunmoahn <mos_dev@naver.com>
2018-12-30 23:20:24 +0900

    Code signing code for production package

--- release-sample.sh
+++ release-sample.sh
@@ -1,4 +1,9 @@
 export GH_TOKEN="<USER_GITHUB_TOKEN>"
 # https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
 
+// optionally
+// code signing for MacOS
+export CSC_LINK="<key file path>"
+export CSC_KEY_PASSWORD="<key file password>"
+
 yarn package
